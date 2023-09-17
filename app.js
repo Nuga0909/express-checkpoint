fbc7fe24c8b849e9118a98267aa15e871bd5f631
Adeshina Adenuga <Nuga0909@gmail.com>
2023-09-17 10:25:46 +0100

    edit app.js to route properly

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var servicesRouter = require('./routes/services');
+var contactRouter = require('./routes/contact');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -20,7 +22,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/services', servicesRouter);
+app.use('/contact', contactRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
