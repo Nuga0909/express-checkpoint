d7abebe5a0dc39fc6a2d6b8e0782fac699ac2823
Adeshina Adenuga <Nuga0909@gmail.com>
2023-09-17 10:34:43 +0100

    fix routing problem to services and contactus

--- routes/services.js
+++ routes/services.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET our services page. */
-router.get('/services', function (req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('services', { title: 'Our Services' });
 });
 
